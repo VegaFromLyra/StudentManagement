59f40251cd4015b6d575c8ab821be81a7b160912
VegaFromLyra <ashab@Ashas-MBP.home>
2015-03-25 16:38:15 -0400

    Refactor directory structure
    Put templates, views, scripts and css files into their own folders

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,27 +2,29 @@ var app = angular.module('myApp', []).
 config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {
         controller: 'HomeController',
-        templateUrl: 'home.html'
+        templateUrl: 'templates/home.html'
     });
         $routeProvider.when('/signUp', {
         controller: 'SignUpController',
-        templateUrl: 'signUp.html'
+        templateUrl: 'templates/signUp.html'
     });
         $routeProvider.when('/signIn', {
         controller: 'SignInController',
-        templateUrl: 'signIn.html'
+        templateUrl: 'templates/signIn.html'
     });
         $routeProvider.when('/dashboard', {
         controller: 'DashboardController',
-        templateUrl: 'dashboard.html'
+        templateUrl: 'templates/dashboard.html'
     });
         $routeProvider.when('/registerStudent', {
         controller: 'RegistrationController',
-        templateUrl: 'registerStudent.html'
+        templateUrl: 'templates/registerStudent.html'
     });
         $routeProvider.when('/deregisterStudent', {
         controller: 'RegistrationController',
-        templateUrl: 'deRegisterStudent.html'
+        templateUrl: 'templates/deRegisterStudent.html'
     })
         .otherwise({redirectTo: '/'});
 }]);
+
+Parse.initialize("KGxHRY1i2W1plkO5rWORg8YQLaKjcwTvs9BIpjyj", "poeOwB2Rw12qXJh90fsXkRhfCBgTIDkQzvb9B0JI");
