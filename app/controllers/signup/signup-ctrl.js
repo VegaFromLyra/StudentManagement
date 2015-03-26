2c430ca5db3c6307cd9cf5c5c6f16d5c5369c1c3
VegaFromLyra <ashab@Ashas-MBP.home>
2015-03-26 15:40:26 -0400

    Put the templates in their area folders
    Put the templates in their area folders

--- app/controllers/signup/signup-ctrl.js
+++ app/controllers/signup/signup-ctrl.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp');
 
 app.controller('SignUpController', ['$scope', '$log', '$location', 'UserService',
     function($scope, $log, $location, userService) {
-        
+
     $scope.signupForm = function() {
 
         if ($scope.signup_form.$valid) {
